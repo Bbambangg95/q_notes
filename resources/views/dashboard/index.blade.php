68e7b05968163ae837ad6aee05ed584fe1374e55
Bbambangg95 <bbambangg95@gmail.com>
2022-04-26 11:40:35 +0700

    mwnambahkan halaman dasboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,13 @@
 @extends('dashboard.layouts.main')
 @section('container')
-<!-- [ Main Content ] end -->
+    <div class='card p-3 m-2'>
+
+        <h5>Ziyadah terbanyak bulan ini</h5>
+        
+        <ul>
+            <li>Ujang Sitohang</li>
+            <li>Ahmad Sukarni</li>
+            <li>Jujun Junaedi</li>
+        </ul>
+    </div>
 @endsection
